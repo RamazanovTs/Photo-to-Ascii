830e2f9a5903d8f9e30348f7ea089638b8333533
Hikmet Ramazanov <hikmetramazanov@outlook.com>
2024-08-29 22:25:49 +0400

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,19 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import tkinter as tk
 from tkinter import filedialog
 
 root = tk.Tk()
 root.withdraw()
 
+replace = ['.', ':', '+', '*', '%', 'S', '#', '@']
+
 file_path = filedialog.askopenfilename()
 
 while not file_path:
     file_path = filedialog.askopenfilename()
 
-
 image = Image.open(file_path)
+image = ImageOps.grayscale(image)
 image = image.resize((100, 100))
 
 width, height = image.size
@@ -21,11 +23,25 @@ ascii_art = []
 for i in range(height):
     row = ""
     for j in range(width):
-        pixel_val = image.getpixel((j, i))
-        if pixel_val[0] == 255 and pixel_val[1] == 255 and pixel_val[2] == 255:
-            row += "*"
+        value = image.getpixel((j, i)) 
+
+        if value <= 31:
+            row += replace[7]
+        elif 32 <= value <= 63:
+            row += replace[6]
+        elif 64 <= value <= 95:
+            row += replace[5]
+        elif 96 <= value <= 127:
+            row += replace[4]
+        elif 128 <= value <= 159:
+            row += replace[3]
+        elif 160 <= value <= 191:
+            row += replace[2]
+        elif 192 <= value <= 223:
+            row += replace[1]
         else:
-            row += "#"
+            row += replace[0]
+
     ascii_art.append(row)
 
 with open("ascii_output.txt", "w") as f:
